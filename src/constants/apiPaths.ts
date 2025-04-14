f2b381c15e3e14d2598453f2fc1f7153746e7e5a
KazikKluz <kazik.kluz@gmail.com>
2025-04-14 11:35:10 +0100

    fix: endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: 'https://pe3emabxdh.execute-api.eu-west-1.amazonaws.com/prod',
-  order: 'https://93gyif6usd.execute-api.eu-west-1.amazonaws.com/prod/api',
-  import: 'https://7nnv07b516.execute-api.eu-west-1.amazonaws.com/prod',
+  order: 'http://kazikkluz-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
+  import: 'http://7nnv07b516.execute-api.eu-west-1.amazonaws.com/prod',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://93gyif6usd.execute-api.eu-west-1.amazonaws.com/prod/api',
+  cart: 'http://kazikkluz-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
